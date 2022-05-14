9c327e9b1b1047f4aa112bdd3031ffa2b5a8fd86
hayrialtnbs <hayrialtunbas0818@gmail.com>
2022-05-14 08:22:18 +0300

    Update

--- Src/Pages/Ayarlar/Ayarlar.js
+++ Src/Pages/Ayarlar/Ayarlar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, FlatList,StatusBar } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StatusBar } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from '../../Styles/AyarlarStyles/Ayarlar.styles';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -8,83 +8,83 @@ import Colors from '../../Utils/Colors';
 
 const Ayarlar = (props) => {
 
-    const gizlilik = () => {
-        props.navigation.navigate('Gizlilik')
-    }
+    // const gizlilik = () => {
+    //     props.navigation.navigate('Gizlilik')
+    // }
 
     const kullanmaKılavuzu = () => {
         props.navigation.navigate('Kullanma Kılavuzu')
     }
 
-    const apasHakkında = () => {
-        props.navigation.navigate('About Apas')
-    }
+    // const apasHakkında = () => {
+    //     props.navigation.navigate('About Apas')
+    // }
 
-    const bilgilendirmeFormu = () => {
-        props.navigation.navigate('İnformation Form')
-    }
-    const kullanıcıSözleşmesi = () => {
-        props.navigation.navigate('User Agreement')
-    }
-    const verilerinKorunması = () => {
-        props.navigation.navigate('Data Protection')
-    }
-    const cerezPolitikası = () => {
-        props.navigation.navigate('Cookie Policy')
-    }
-    const bizeUlaşın = () => {
-        props.navigation.navigate('Contact Us')
-    }
+    // const bilgilendirmeFormu = () => {
+    //     props.navigation.navigate('İnformation Form')
+    // }
+    // const kullanıcıSözleşmesi = () => {
+    //     props.navigation.navigate('User Agreement')
+    // }
+    // const verilerinKorunması = () => {
+    //     props.navigation.navigate('Data Protection')
+    // }
+    // const cerezPolitikası = () => {
+    //     props.navigation.navigate('Cookie Policy')
+    // }
+    // const bizeUlaşın = () => {
+    //     props.navigation.navigate('Contact Us')
+    // }
 
     const Data = [
+        // {
+        //     id: "1.",
+        //     icon: "security",
+        //     name: "Confidentiality Agreement",
+        //     onpress: () => gizlilik()
+        // },
         {
             id: "1.",
-            icon: "security",
-            name: "Confidentiality Agreement",
-            onpress: () => gizlilik()
-        },
-        {
-            id: "2.",
             iconName: "book-reader",
             name: "Read Me",
             onpress: () => kullanmaKılavuzu()
         },
-        {
-            id: "3.",
-            icon: "announcement",
-            name: "About Apas",
-            onpress: () => apasHakkında()
-        },
-        {
-            id: "4.",
-            iconName: "info-circle",
-            name: "İnformation Form",
-            onpress: () => bilgilendirmeFormu()
-        },
-        {
-            id: "5.",
-            icon: "assignment",
-            name: "User Agreement",
-            onpress: () => kullanıcıSözleşmesi()
-        },
-        {
-            id: "6.",
-            icon: "question-answer",
-            name: "Data Protection",
-            onpress: () => verilerinKorunması()
-        },
-        {
-            id: "7.",
-            icon: "policy",
-            name: "Cookie Policy",
-            onpress: () => cerezPolitikası()
-        },
-        {
-            id: "8.",
-            icon: "support-agent",
-            name: "Contact Us",
-            onpress: () => bizeUlaşın()
-        },
+        // {
+        //     id: "3.",
+        //     icon: "announcement",
+        //     name: "About Apas",
+        //     onpress: () => apasHakkında()
+        // },
+        // {
+        //     id: "4.",
+        //     iconName: "info-circle",
+        //     name: "İnformation Form",
+        //     onpress: () => bilgilendirmeFormu()
+        // },
+        // {
+        //     id: "5.",
+        //     icon: "assignment",
+        //     name: "User Agreement",
+        //     onpress: () => kullanıcıSözleşmesi()
+        // },
+        // {
+        //     id: "6.",
+        //     icon: "question-answer",
+        //     name: "Data Protection",
+        //     onpress: () => verilerinKorunması()
+        // },
+        // {
+        //     id: "7.",
+        //     icon: "policy",
+        //     name: "Cookie Policy",
+        //     onpress: () => cerezPolitikası()
+        // },
+        // {
+        //     id: "8.",
+        //     icon: "support-agent",
+        //     name: "Contact Us",
+        //     onpress: () => bizeUlaşın()
+        // },
     ]
     return (
         <LinearGradient
